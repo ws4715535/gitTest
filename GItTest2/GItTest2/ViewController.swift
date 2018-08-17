f3f20d646886315651ee0fcb8d5a26a4d2e7ddc2
ws <ws4715535@foxmail.com>
2018-08-17 21:14:08 +0800

    init

---
+++ GItTest2/GItTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GItTest2
+//
+//  Created by ws on 2018/8/17.
+//  Copyright © 2018年 ws. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
